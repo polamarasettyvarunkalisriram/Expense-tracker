5ab78deb0365178263a25e58a79fe34fdc3cea8f
polamarasettyvarunkalisriram <srirampksrirampk@gmail.com>
2026-05-19 10:53:09 +0530

    first commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,17 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import { ClerkProvider } from '@clerk/clerk-react'
 import ExpenseProvider from './context/Expensecontext'
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <ExpenseProvider>
         <App />
       </ExpenseProvider>
-    </ClerkProvider>
   </React.StrictMode>
 )
