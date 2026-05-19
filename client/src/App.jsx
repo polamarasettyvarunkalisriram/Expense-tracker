a0e05cffbe9d9f757d5453b6b980ecbb91aaee42
polamarasettyvarunkalisriram <srirampksrirampk@gmail.com>
2026-05-19 10:56:12 +0530

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import Home from './Home'
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Add from './Add'
 import Edit from './Edit'
 import Login from './Login'
-
-import { SignedIn, SignedOut } from '@clerk/clerk-react'
 
 const App = () => {
   return (
@@ -14,22 +12,10 @@ const App = () => {
 
         <Route path='/' element={<Login />} />
 
-        <Route
-          path='/home'
-          element={
-            <>
-              <SignedIn>
-                <Home />
-              </SignedIn>
-
-              <SignedOut>
-                <Navigate to="/" />
-              </SignedOut>
-            </>
-          }
-        />
+        <Route path='/home' element={<Home />} />
 
         <Route path='/add' element={<Add />} />
+
         <Route path="/edit/:id" element={<Edit />} />
 
       </Routes>
--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,32 +1,8 @@
 import React from 'react'
-import {
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/clerk-react'
-
-import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
-    <div>
-
-      <SignedOut>
-        <SignInButton mode="modal">
-          <button>Sign In To Expense Tracker</button>
-        </SignInButton>
-      </SignedOut>
-
-      <SignedIn>
-        <UserButton />
-
-        <Link to="/home">
-          <button>Go To Home</button>
-        </Link>
-      </SignedIn>
-
-    </div>
+    <div>Login</div>
   )
 }
 
